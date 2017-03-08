d99c715b43d5b6c14cad94988487432a9c752176
Petru (Armand) Bancila <petru.bancila@kcl.ac.uk>
2017-03-08 23:23:29 +0000

    Stop child click propagation
    This will let you select the contact info text from the Contacts window without triggering the window's click event listener that switches to the other window.

--- script.js
+++ script.js
@@ -3,4 +3,7 @@ $(document).ready(function () {
   $('#card').click(function(){
     $("#email").text("armandbancila@gmail.com");
 });
+  $(".back").children().click(function(e){
+    e.stopPropagation();
+  });
 });
