294eaad3fb7b95f41acff02168b658b26adb4bc1
Petru (Armand) Bancila <petru.bancila@kcl.ac.uk>
2017-06-20 20:38:08 +0300

    Change colours

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
   $("#card").flip();
-  $('#card').click(function(){
-    var x = "armandbancila" /*random comment so that the stupid spambots don't find out that this is my email*/+ "@" + "gmail" + "." + "com";
+  $('#card').click(function () {
+    var x = "armandbancila" /*random comment so that the stupid spambots don't find out that this is my email*/ + "@" + "gmail" + "." + "com";
     $("#email").text(x);
-});
-  $(".back").children().click(function(e){
+  });
+  $(".back").children().click(function (e) {
     e.stopPropagation();
   });
 });
